313d92849b8cc6b0cd279791f786907e40b1f4b9
Jason J Wesley <roninleonhart9783@gmail.com>
2015-09-24 11:30:38 -0500

    HelloWorld.java
    My First Program

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+* This is an example class that illustrates printing a message to the screen
+*
+*@author Jason J Wesley
+*/
+public class HelloWorld {
+   /**
+   *starts the program
+   *
+   *@param args
+   *command line arguments
+   */
+   public static void main(String[] args) {
+      System.out.println("Hello World!");
+	}
+}
